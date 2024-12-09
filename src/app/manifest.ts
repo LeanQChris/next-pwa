8884c12d14bc086e16c8138c5155a2ab3737e1c7
LeanQChris <chris.leanq@gmail.com>
2024-12-09 16:04:39 +0545

    changed name and logo for manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,23 +2,23 @@ import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
     return {
-        name: 'Next.js PWA',
-        short_name: 'NextPWA',
-        description: 'A Progressive Web App built with Next.js',
+        name: 'FilterMax',
+        short_name: 'FilterMax',
+        description: 'All Filters for all machines Filters, Filter kits and accessories for all makes and models',
         start_url: '/',
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#000000',
         icons: [
             {
-                src: '/next.svg',
+                src: '/logo.png',
                 sizes: '192x192',
-                type: 'image/svg+xml',
+                type: 'image/png',
             },
             {
-                src: '/next.svg',
+                src: '/logo.png',
                 sizes: '512x512',
-                type: 'image/svg+xml',
+                type: 'image/png',
             },
         ],
     }
