d40e4e5499fd471996eb3f3e34f764048234c76c
LeanQChris <chris.leanq@gmail.com>
2024-12-10 13:05:23 +0545

    fix build

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const protectedRoutes = ['/', '/profile']
 const publicRoutes = ['/login']
@@ -7,6 +7,10 @@ export default async function middleware(req: NextRequest) {
     const path = req.nextUrl.pathname
     const isProtectedRoute = protectedRoutes.includes(path)
     const isPublicRoute = publicRoutes.includes(path)
+
+    console.log(isProtectedRoute, isPublicRoute)
+
+    return NextResponse.next();
 }
 
 export const config = {
